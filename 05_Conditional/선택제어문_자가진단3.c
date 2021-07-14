17abd4efd3d4b235fe0933bb4439f14b8b4771a3
kit-lec <kit_lec@hotmail.com>
2021-07-14 15:49:40 +0900

    210714-6교시

--- 05_Conditional/선택제어문_자가진단3.c
+++ 05_Conditional/선택제어문_자가진단3.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
+#pragma warning(disable:4996)
 
 /*
-
+나이를 입력받아 20살 이상이면 "adult"라고 출력하고
+그렇지 않으면 몇 년후에 성인이 되는지를 "○ years later"라는
+메시지를 출력하는 프로그램을 작성하시오.
 */
-
 int main()
 {
-	// TODO
+	int age;
+	scanf("%d", &age);
 
-	printf("\nENTER 를 누르면 종료합니다\n");
+	if (age >= 20)
+		printf("adult");
+	else
+		printf("%d years later", 20 - age);
+
+	getchar();
+	printf("\nENTER 누르면 프로그램을 종료합니다\n");
 	getchar();
 	return 0;
 }
