e5f5dc16f02b2c203527433079140d7caff54ef2
kit-lec <kit_lec@hotmail.com>
2021-07-16 16:49:04 +0900

    210716-7교시

--- 10_String/1001_string.c
+++ 10_String/1001_string.c
@@ -42,7 +42,115 @@
 
 int main()
 {
-	// TODO
+	char *s1 = "Hello";    // 포인터 s1 에 문자열 "Hello" 의 주소 저장
+	char s2[10] = "Hello";  // 길이가 10인 char[] 을 선언하고 문자열로 초기화
+
+	printf("s1 = %s, s2 = %s\n", s1, s2);
+
+	s2[0] = 'K';
+	printf("s2 = %s\n", s2);
+
+	//s1[0] = 'K';  // 에러! , "Hello" 는 리터럴 상수.  변경 불가
+
+	// 문자열의 길이 strlen()
+	{ 
+		printf("%d\n", strlen(s1));
+		printf("%d\n", strlen(s2));
+	}
+
+	// 문자열 비교 strcmp()
+	{
+		int ret = strcmp(s1, s2);  // "Hello" < "Kello"
+		printf("%d\n", ret);  // -1
+
+		if (strcmp(s1, s2)) {
+			printf("%s 와 %s 는 다릅니다\n", s1, s2);
+		}
+		else {
+			printf("%s 와 %s 는 같습니다\n", s1, s2);
+		}
+
+		printf("%d\n", strcmp("aaa", "aaa"));  // 0
+		printf("%d\n", strcmp("aab", "aaa"));  // 1
+		printf("%d\n", strcmp("aab", "aac"));  // -1
+		printf("%d\n", strcmp("aAaA", "AaAa"));  // 1
+	}
+
+	// 문자열 복사 strcpy()
+	{
+		char s1[10] = "Hello";
+		char s2[10];
+
+		//s2 = "Hello";  // 안되요..
+
+		printf("strcpy() 전 s2 = %s\n", s2);
+
+		strcpy(s2, s1);  // s1 의 문자열을 s2 로 복사
+		printf("strcpy() 후 s2 = %s\n", s2);
+	}
+
+	{
+		// 주의
+		char *s1 = "Hello";  // 리터럴 상수를 가리키는 포인터 s1, s2
+		char *s2 = "";
+
+		// strcpy(s2, s1);  // 에러
+	}
+
+	// 문자열 연결 strcat()  string concatenation
+	{
+		char s1[10] = "world";
+		char s2[20] = "Hello";
+
+		printf("strcat() 실행전 s2 = %s\n", s2);
+
+		strcat(s2, s1);  // s2 뒤에 s1을 붙임
+
+		printf("strcat() 실행후 s2 = %s\n", s2);
+
+	}
+
+	//  "문자열" 안에서 '문자'로 검색 strchr()
+	{
+		char s1[30] = "A Garden Diary";
+		char *ptr = strchr(s1, 'a');
+
+		if (ptr) {
+			printf("%d 번째에서 찾았다\n", ptr - s1);
+		}
+		else {
+			printf("없다\n");
+		}
+
+		printf("ptr = %s\n", ptr);
+
+		while (ptr != NULL)  // 더이상 검색된 '문자' 가 없을때까지 반복 검색
+		{
+			printf("%d 번째에서 발견 %s\n", ptr - s1, ptr);
+			ptr = strchr(ptr + 1, 'a');  // 발견한 다음부터 다시 검색
+		}
+
+	}
+
+
+	// 문자열 안에서 문자열로 검색 strstr()
+	{
+		char s1[30] = "A Garden Diary";
+
+		char *ptr = strstr(s1, "den");  // 문자열 s1 에서 "den" 을 검색
+
+		if (ptr) {
+			printf("strstr() 발견 %d번째, %s\n", ptr - s1, ptr);
+		}
+		else {
+			printf("strstr() 발견못함\n");
+		}
+
+	}
+
+
+
+
 
 	printf("\nENTER 를 누르면 종료합니다\n");
 	getchar();
